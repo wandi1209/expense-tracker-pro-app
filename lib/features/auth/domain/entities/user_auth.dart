4083718d742d61aa7f8032061271dac327619ae5
wandi1209 <wandider@gmail.com>
2025-04-30 06:44:43 +0700

    feat(auth): create UserAuth model

---
+++ lib/features/auth/domain/entities/user_auth.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class UserAuth extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String confirmPassword;
+
+  const UserAuth({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.confirmPassword,
+  });
+
+  @override
+  List<Object?> get props => [name, email, password, confirmPassword];
+}
