cc84c6967d9a2b6c3745c9ca7f5c84f1d33d7a90
wandi1209 <wandider@gmail.com>
2025-05-01 06:48:31 +0700

    feat(auth): create login model for request and response

---
+++ lib/features/auth/data/models/login_req.dart
@@ -0,0 +1,16 @@
+class RegisterReqModel {
+  final String email;
+  final String password;
+
+  RegisterReqModel({
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+}
