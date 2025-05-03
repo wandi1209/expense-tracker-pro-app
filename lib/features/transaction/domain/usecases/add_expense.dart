1ebb9eef31b1cd55f95786d41b28adf780aa4b01
wandi1209 <wandider@gmail.com>
2025-05-03 08:03:42 +0700

    feat(transaction): create addExpense usecase

--- lib/features/transaction/domain/usecases/add_expense.dart
+++ lib/features/transaction/domain/usecases/add_expense.dart
@@ -1,9 +1,9 @@
 import 'package:expense_tracker_pro/features/transaction/domain/repositories/transaction_repository.dart';
 
-class AddIncome {
+class AddExpense {
   final TransactionRepository transactionRepository;
 
-  AddIncome({required this.transactionRepository});
+  AddExpense({required this.transactionRepository});
 
   Future<Map<String, dynamic>> execute(int amount, String remarks) async {
     return await transactionRepository.addIncome(amount, remarks);
