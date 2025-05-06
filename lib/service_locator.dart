e74f7c46a3ef9075a3b3a9ed6fa25692e59562b4
wandi1209 <wandider@gmail.com>
2025-05-07 06:54:33 +0700

    feat(auth): using get it for injection

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,4 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:expense_tracker_pro/features/auth/data/datasources/remote_datasource.dart';
+import 'package:expense_tracker_pro/features/auth/data/repositories/user_auth_repository_implementation.dart';
+import 'package:expense_tracker_pro/features/auth/domain/repositories/user_auth_repository.dart';
+import 'package:expense_tracker_pro/features/auth/domain/usecases/forgot_password.dart';
+import 'package:expense_tracker_pro/features/auth/domain/usecases/login.dart';
+import 'package:expense_tracker_pro/features/auth/domain/usecases/register.dart';
+import 'package:expense_tracker_pro/features/auth/domain/usecases/reset_password.dart';
+import 'package:expense_tracker_pro/features/auth/presentation/bloc/authentication_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 
@@ -7,4 +15,35 @@ var locator = GetIt.instance;
 Future<void> init() async {
   final Dio dio = Dio(BaseOptions(baseUrl: '${dotenv.env["BASE_URL"]}'));
   locator.registerLazySingleton(() => dio);
+
+  // Feature - Auth
+  // BLOC
+  locator.registerFactory(
+    () => AuthenticationBloc(
+      register: locator(),
+      login: locator(),
+      forgotPassword: locator(),
+      resetPassword: locator(),
+    ),
+  );
+
+  // USECASE
+  locator.registerLazySingleton(() => Register(userAuthRepository: locator()));
+  locator.registerLazySingleton(() => Login(userAuthRepository: locator()));
+  locator.registerLazySingleton(
+    () => ForgotPassword(userAuthRepository: locator()),
+  );
+  locator.registerLazySingleton(
+    () => ResetPassword(userAuthRepository: locator()),
+  );
+
+  // REPOSITORY
+  locator.registerLazySingleton<UserAuthRepository>(
+    () => UserAuthRepositoryImplementation(remoteDatasource: locator()),
+  );
+
+  // DATASOURCE
+  locator.registerLazySingleton<UserAuthRemoteDatasource>(
+    () => UserAuthRemoteDatasourceImplementation(dio),
+  );
 }
