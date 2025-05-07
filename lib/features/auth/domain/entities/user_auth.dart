2be77a7b17aa77abafb99e3fd0d47a8ff77a4d39
wandi1209 <wandider@gmail.com>
2025-05-07 14:56:59 +0700

    feat(auth): create accessToken from required to optional variabel

--- lib/features/auth/domain/entities/user_auth.dart
+++ lib/features/auth/domain/entities/user_auth.dart
@@ -4,10 +4,14 @@ class UserAuth extends Equatable {
   final String id;
   final String name;
   final String email;
-  final String accesToken;
+  final String? accesToken;
 
-  const UserAuth({required this.id, required this.name, required this.email, required this.accesToken});
-
+  const UserAuth(
+    this.accesToken, {
+    required this.id,
+    required this.name,
+    required this.email,
+  });
 
   @override
   List<Object?> get props => [id, name, email, accesToken];
