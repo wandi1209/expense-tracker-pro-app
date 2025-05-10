410b197aed24fc25fc9211efc3c7450208bd7482
wandi1209 <wandider@gmail.com>
2025-05-10 19:44:51 +0700

    feat(statistic): add logic for statistic state

--- lib/features/statistic/presentation/bloc/statistic_state.dart
+++ lib/features/statistic/presentation/bloc/statistic_state.dart
@@ -1,9 +1,40 @@
 part of 'statistic_bloc.dart';
 
-abstract class StatisticState extends Equatable {
-  const StatisticState();  
+abstract class StatisticState extends Equatable {}
+
+class StatisticInitial extends StatisticState {
+  @override
+  List<Object?> get props => [];
+}
+
+class StatisticEventLoading extends StatisticState {
+  @override
+  List<Object?> get props => [];
+}
+
+class StatisticGetByFilterSuccess extends StatisticState {
+  final List<TransactionModel> transactions;
+
+  StatisticGetByFilterSuccess({required this.transactions});
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [transactions];
 }
-class StatisticInitial extends StatisticState {}
+
+class StatisticGetTopTransactionsSuccess extends StatisticState {
+  final List<TransactionModel> topTransactions;
+
+  StatisticGetTopTransactionsSuccess({required this.topTransactions});
+
+  @override
+  List<Object?> get props => [topTransactions];
+}
+
+class StatisticEventFailure extends StatisticState {
+  final String error;
+
+  StatisticEventFailure({required this.error});
+
+  @override
+  List<Object?> get props => [error];
+}
