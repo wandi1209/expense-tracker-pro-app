4fbad6c847cf291807d11b3b61c0b67d73655f2d
wandi1209 <wandider@gmail.com>
2025-05-11 06:40:56 +0700

    fix: after login and register cannot send token to header

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -23,7 +23,6 @@ import 'package:expense_tracker_pro/features/transaction/domain/usecases/delete_
 import 'package:expense_tracker_pro/features/transaction/domain/usecases/edit_transaction.dart';
 import 'package:expense_tracker_pro/features/transaction/domain/usecases/get_transaction.dart';
 import 'package:expense_tracker_pro/features/transaction/presentation/bloc/transaction_bloc.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 
@@ -31,18 +30,28 @@ var locator = GetIt.instance;
 
 Future<void> init() async {
   final authService = AuthService();
-  String? token = await authService.getToken();
-  debugPrint("token nih : ${token}");
 
   final Dio dio = Dio(
     BaseOptions(
       baseUrl: '${dotenv.env["BASE_URL"]}',
-      headers: token != null ? {'Authorization': 'Bearer $token'} : null,
       validateStatus: (status) {
-        return status! < 500; // Menerima semua status di bawah 500
+        return status! < 500;
       },
     ),
   );
+
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        String? token = await authService.getToken();
+        if (token != null) {
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+        return handler.next(options);
+      },
+    ),
+  );
+
   locator.registerLazySingleton(() => dio);
 
   // Feature - Auth
